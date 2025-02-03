431c1d4ccd47cd075131ee21534d5e91d8867b7d
Justin Cox <justin.cox@lltc.edu>
2025-02-03 13:57:54 -0600

    playing with dir

---
+++ lessons/links.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Links";
+$DIR = __DIR__;
+
+include_once($DIR .'/templates/head.php');
+include_once($DIR . '/nav.php');
+?>
+<div class="container">
+
+</div>
+<?php include_once($DIR . '/footer.php'); ?>
